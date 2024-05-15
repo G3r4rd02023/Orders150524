41074cbb51a1a3afe813dd8716d3c62fb4b7b321
Gerardo Lanza <glanza007@gmail.com>
2024-05-15 16:40:11 -0600

    create table countries

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+
 namespace Orders.Backend
 {
     public class Program
@@ -13,7 +16,7 @@ namespace Orders.Backend
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
