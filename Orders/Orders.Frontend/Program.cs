1056b038333766c914909bd174ab0d5c88acdbad
Gerardo Lanza <glanza007@gmail.com>
2024-05-15 19:17:00 -0600

    begin repository pattern

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Orders.Frontend.Repositories;
 
 namespace Orders.Frontend
 {
@@ -11,7 +12,8 @@ namespace Orders.Frontend
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7142/") });
+            builder.Services.AddScoped<IRepository, Repository>();
 
             await builder.Build().RunAsync();
         }
